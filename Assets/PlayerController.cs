8c548531e16dd06763a651eece9c505505fcf2b3
John-LukeD <jdeneen1@uncc.edu>
2024-10-03 15:49:41 -0400

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    MovementController movementController;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        movementController = GetComponent<MovementController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movementController.setDirection("left");
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movementController.setDirection("right");
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            movementController.setDirection("up");
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movementController.setDirection("down");
+        }
+    }
+}
